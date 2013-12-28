f3f0cb5eb981adf15ca33bef9abdbee6a9884180
Umut Bozkurt <umut9273@hotmail.com>
2013-12-28 13:33:00 +0200

    no message

---
+++ Formatter_ExampleProject/CustomCell.m
@@ -0,0 +1,41 @@
+//
+//  CustomCell.m
+//  FormatterSampleProject
+//
+//  Created by Wissen on 28/12/13.
+//  Copyright (c) 2013 Umut Bozkurt. All rights reserved.
+//
+
+#import "CustomCell.h"
+#import "Formatter.h"
+
+@implementation CustomCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self)
+    {
+        Formatter *f = [[Formatter alloc] initWithWorkspace:self withController:self];
+        
+        self.label.text = @"asdasd";
+        self.view.backgroundColor = [UIColor redColor];
+        
+        [f addConstraint:[Constraint constraintView:self.label toSuperViewEdge:SuperViewTopEdge withSize:40 withDistance:10 withRelation:RelationTypeEqualTo withSizeType:SizeTypeEqualTo]];
+        [f addConstraint:[Constraint constraintView:self.view ToView:self.label withSize:40 withDistance:10 withDirection:LayoutDirectionHorizontal withRelation:20 withSizeType:SizeTypeEqualTo]];
+        [f addConstraint:[Constraint constraintView:self.view toSuperViewEdge:SuperViewTopEdge withSize:40 withDistance:10 withRelation:RelationTypeEqualTo withSizeType:SizeTypeEqualTo]];
+        [f addConstraint:[Constraint constraintView:self.label toSuperViewEdge:SuperViewLeftEdge withSize:30 withDistance:10 withRelation:RelationTypeEqualTo withSizeType:SizeTypeEqualTo]];
+        
+        [f applyConstraints];
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ Formatter_ExampleProject/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  FormatterSampleProject
+//
+//  Created by Wissen on 28/12/13.
+//  Copyright (c) 2013 Umut Bozkurt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
